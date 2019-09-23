554d953fa2d0b59313273cd63c424b1621e646d5
LoicBrassart <loic.brassart@gmail.com>
2019-09-23 18:58:40 +0200

    Moved our JS code in a function

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-// Here, we'll try to re-create our sample HTML card
-// We won't update index.html or style.css from now on
-
-// Step1: we select our gallery
 const gallery = document.getElementById("gallery");
 
-// Step2: we *create* a new <img> tag. We afterwards add a "src" attribute and the "background" class to it
-const background = document.createElement("img");
-background.setAttribute("src", "img/backgrounds/outdoor-1.jpg");
-background.classList.add("background");
+// We wrap our code in a function, to be able to call it easily
+// The first parameter (name) must match the folder name
+const appendChar = name => {
+  const background = document.createElement("img");
+  background.setAttribute("src", "img/backgrounds/outdoor-1.jpg");
+  background.classList.add("background");
 
-// Step3: same thing for a figure we want to add on top of our background
-const figure = document.createElement("img");
-figure.setAttribute("src", `img/adventurers/amisandra/1-f.png`);
-figure.classList.add("figure");
+  const figure = document.createElement("img");
+  figure.setAttribute("src", `img/adventurers/${name}/1-f.png`);
+  figure.classList.add("figure");
 
-// Step4: rinse and repeat with our footer
-const footer = document.createElement("footer");
-footer.innerHTML = "<h3>A'misandra</h3>";
+  const footer = document.createElement("footer");
+  footer.innerHTML = `<h3>${name}</h3>`;
 
-// Step5: now we create our <article> tag, which will hold all the other elements
-const character = document.createElement("article");
-character.classList.add("character");
+  const character = document.createElement("article");
+  character.classList.add("character");
 
-// Step6: we attach our background, our figure and our footer to the article
-character.appendChild(background);
-character.appendChild(figure);
-character.appendChild(footer);
+  character.appendChild(background);
+  character.appendChild(figure);
+  character.appendChild(footer);
+  gallery.appendChild(character);
+};
 
-//step7: finally, we attach our brand new character to our gallery
-gallery.appendChild(character);
+appendChar("amisandra");
+// Problem: The character "A'misandra" is now called "amisandra", which bothers me :(
+// We'll have to fix this very soon!
